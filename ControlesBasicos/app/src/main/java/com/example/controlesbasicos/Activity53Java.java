8fef308328869aa713d739f07070c370b8dd04f7
ItsKein <lopezolivajuanjo21@gmail.com>
2025-11-28 09:35:15 +0100

    Actualizaciones

--- ControlesBasicos/app/src/main/java/com/example/controlesbasicos/Activity53Java.java
+++ ControlesBasicos/app/src/main/java/com/example/controlesbasicos/Activity53Java.java
@@ -2,6 +2,7 @@ package com.example.controlesbasicos;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -10,22 +11,31 @@ public class Activity53Java extends AppCompatActivity implements View.OnClickLis
 
     private TextView tvMensaje;
 
-    protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_5_3_on_click);
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_5_3_java);
 
-    public void onClick(View v) {
-        int id = v.getId();
-        tvMensaje = findViewById((R.id.tvMensaje));
-    if (id == R.id.btnBoton1) {
-        tvMensaje.setText(R.string.boton_1_pulsado);
-        tvMensaje.setTextColor(ContextCompat.getColor(Activity53Java.this, R.color.miColorRed));
-    } else if (id == R.id.btnBoton2) {
-        tvMensaje.setText(R.string.boton_2_pulsado);
-        tvMensaje.setTextColor(ContextCompat.getColor(Activity53Java.this, R.color.custom_green));
-    } else {
-        //Codigo por defecto
+        Button btn1 = findViewById(R.id.btnBoton1);
+        btn1.setOnClickListener(this);
+
+        Button btn2 = findViewById(R.id.btnBoton2);
+        btn2.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View v) {
+
+        int id = v.getId(); //obteneos el id
+        //obtenemos las referencias a todas las vistas necesarias
+        tvMensaje = findViewById(R.id.tvMensaje);
+        if (id == R.id.btnBoton1){
+            tvMensaje.setText(R.string.boton_1_pulsado);
+            tvMensaje.setTextColor(ContextCompat.getColor(Activity53Java.this, R.color.miColorRed));
+        }else if (id == R.id.btnBoton2){
+            tvMensaje.setText(R.string.boton_2_pulsado);
+            tvMensaje.setTextColor(ContextCompat.getColor(Activity53Java.this, R.color.custom_green));
+        } else {
+
+        }
 }
 }
--- ControlesBasicos/app/src/main/java/com/example/controlesbasicos/Activity53OnClick.java
+++ ControlesBasicos/app/src/main/java/com/example/controlesbasicos/Activity53OnClick.java
@@ -1,5 +1,6 @@
 package com.example.controlesbasicos;
 
+import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -7,19 +8,18 @@ import androidx.core.content.ContextCompat;
 
 public class Activity53OnClick extends AppCompatActivity {
 
-    private TextView tvMensaje;
-    public void onBotonPulsado (View view) {
-        int id = view.getId();
-        tvMensaje = findViewById((R.id.tvMensaje));
-        if (id == R.id.btnBoton1) {
-            tvMensaje.setText(R.string.boton_1_pulsado);
-            tvMensaje.setTextColor(ContextCompat.getColor(Activity53OnClick.this, R.color.miColorRed));
-        } else if (id == R.id.btnBoton2) {
-            tvMensaje.setText(R.string.boton_2_pulsado);
-            tvMensaje.setTextColor(ContextCompat.getColor(Activity53OnClick.this, R.color.custom_green));
-        } else {
-
-        }
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_5_3_on_click);
     }
 
-}
+    public void onBotonPulsado (View view){
+        TextView tvMensaje = findViewById(R.id.tvMensaje);
+
+        tvMensaje.setText("BOTON PULSADO");
+        tvMensaje.setTextColor(ContextCompat.getColor(Activity53OnClick.this, R.color.custom_green));
+    }
+    }
+
+
