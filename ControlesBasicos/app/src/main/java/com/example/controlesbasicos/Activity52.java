6cc3da2431bccb57a7b00d8103ca58a262d76bd6
ItsKein <lopezolivajuanjo21@gmail.com>
2025-11-28 09:22:43 +0100

    Actualizaciones

---
+++ ControlesBasicos/app/src/main/java/com/example/controlesbasicos/Activity52.java
@@ -0,0 +1,28 @@
+package com.example.controlesbasicos;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity52 extends AppCompatActivity {
+
+    private TextView textoAnimado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_5_2);
+
+        textoAnimado = findViewById(R.id.texto_animado);
+
+        // Cargar la animación que tengas
+        Animation animacion = AnimationUtils.loadAnimation(this, R.anim.animacion);
+
+        // Aplicar animación
+        textoAnimado.startAnimation(animacion);
+    }
+
+}
